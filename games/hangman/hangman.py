91cbf26971a22e16780fb350a24ddb022c548fa2
Elliot Richards <elliot.a.richards@gmail.com>
2025-07-31 23:11:54 +0100

    feat: :sparkles: adding pause to the game

--- games/hangman/hangman.py
+++ games/hangman/hangman.py
@@ -1,6 +1,7 @@
 import random
 import csv
 import string
+import time
 
 '''
 This is a simple hangman game. It started out smaller than this to test logic and flow, and after fix a few bugs I started to add a few extras such as keeping track of the guesses, difficulty level, and a separate
@@ -12,6 +13,10 @@ lives = 0
 
 # set the heart symbol emoji to represent 'lives'
 heart_symbol = u'\u2764'
+
+# create a slight pause in the game to make it easier to follow the messages
+pause = 0.100
+time.sleep(pause)
 
 # create an empty list to store the guessed letters
 guessed_letters = []
@@ -73,17 +78,20 @@ while lives == 0:
         print("You need to select a level to continue.")
 
 while lives > 0:
-    print(f"\nLives left: {lives} " + heart_symbol * lives)
+    time.sleep(pause)
+    print(f"\nLives: {lives} " + heart_symbol * lives)
     print(f"Word: {''.join(clue)}") # join the list to print the word
         
     # Added .lower() to the input() so that the user's guess (whether uppercase or lowercase) is consistently handled, making the game more forgiving.
+    time.sleep(pause)
     guess = input("\nGuess a letter or the whole word: ").lower()
     # append eash guess to the empty list we created
     guessed_letters.append(guess)
-
+    
     # check if the user guessed the entire word correctly
     if guess == random_word:
         guessed_word_correctly = True
+        time.sleep(pause)
         print(f"\nYou won! The secret word was {random_word}!")
         break
     
@@ -91,26 +99,34 @@ while lives > 0:
     # Added a simple check if len(guess) == 1 and guess.isalpha(): to ensure the user is actually entering a single letter if they're not guessing the whole word.
     if len(guess) == 1 and guess.isalpha():
         if guess in random_word:
+            time.sleep(pause)            
             print("\nYou guessed a letter correctly!")
             update_clue(guess, random_word, clue)
             # print a list of guessed letters
-            print(''.join(guessed_letters))
+            time.sleep(pause)
+            print(f"Guessed letters: {''.join(guessed_letters)}")
             if ''.join(clue) == random_word:
                 guessed_word_correctly = True
                 print(''.join(clue)) # show the completed word
+                time.sleep(pause)
                 print(f"\nYou won! The random word was {random_word}!")
                 break
         else:
+            time.sleep(pause)
             print("\nIncorrect, you lose a life.")
             # print a list of guessed letters
-            print(''.join(guessed_letters))
+            time.sleep(pause)
+            print(f"Guessed letters: {''.join(guessed_letters)}")
             lives -= 1
     else:
         print("\nInvalid guess. Please guess a single letter or the whole word.")
+        time.sleep(pause)
 
     if lives == 0:
         print(f"\nYou lost all your lives! The secret word was {random_word}!")
+        time.sleep(pause)
         break
                     
+time.sleep(pause)
 print("\nThanks for playing!")
 
